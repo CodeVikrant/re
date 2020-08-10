a900181c80cff6e2f997af77c766085bdd0b4250
CodeVikrant <63932256+CodeVikrant@users.noreply.github.com>
2020-08-10 23:18:12 +0530

    Add initial cv website files

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">    
+    <title>offical.vikrant</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="img-div">
+    </div>
+
+    <div class="content">
+    <h5>HI There!</h5>
+    <h1>I'M  <span>VIKRANT PANWAR</span></h1>
+    <p>I'm a Tunisian based web designer & front‑end developer <br>
+    focused on crafting clean & user‑friendly experiences, <br>
+    I am passionate about building excellent software that <br>
+    improves the lives of those around me.</p>
+    <a href="http://localhost:8888/myself%20website%20/more.php"><input type="button" value="More About Me"></a>
+    
+    
+    </div>
+</body>
+</html>
---
+++ more.php
@@ -0,0 +1,19 @@
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>offical.vikrant</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="main">
+    <h1>More to Come...</h1>
+
+
+    </div>
+</body>
+</html>
